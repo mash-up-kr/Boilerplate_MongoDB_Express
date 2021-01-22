2f865f8d12974403880110d53a4ffb9dad6a6106
HyunLee <olo5112olo@naver.com>
2021-01-23 03:15:54 +0900

    [fix]: error handler에서 오류가 안나도록 수정한다 (#19)
    * [docs]: README 문서를 업데이트한다.
    
    * [refactor]: comment 모듈로 이름을 수정한다.
    
    * [chore]: .gitignore에 파일을 추가한다
    
    * [fix]: error handler에서 오류가 안나도록 수정했습니다.
    
    view 엔진이 없는 상태에서 err를 error 페이지로 전송하여 발생하는 에러를 발견했습니다.
    json의 형태로 에러를 보내도록 수정했습니다.

--- app.js
+++ app.js
@@ -32,12 +32,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({
+    message: err.message,
+    error 
+  });
 });
 
 export default app;
